4a401bb44db65bb2636e2e775f50627593911edc
posthumousplay <57947424+posthumousplay@users.noreply.github.com>
2023-03-31 10:11:01 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,53 @@
+questions = [
+    {
+        "question": "South beliefs is true beliefs..",
+        "effect": {
+            "a": 10,
+            "b": 10,
+            "c": 10,
+            "d": 10,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0,
+            "j": 0,
+            "k": 0
+        }
+    },    
+  {
+        "question": "Left-wing beliefs can change world in good way.",
+        "effect": {
+            "a": 20,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 10,
+            "f": 10,
+            "x": 10,
+            "g": 10,
+            "h": 10,
+            "i": 10,
+            "j": 0,
+            "k": 0
+        }
+    },
+      {
+        "question": "Rotation is better than mainstream beliefs.",
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "x": 0,
+            "g": 0,
+            "h": 0,
+            "i": 0,
+            "j": 10,
+            "k": 10
+        }
+    },
+  };
